48ffc4682fa1744a8c85b79461f46d35b65c4837
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-11-06 23:32:24 +0500

    fixed bg names

--- src/components/CompanyLayout.jsx
+++ src/components/CompanyLayout.jsx
@@ -12,16 +12,16 @@ const CompanyLayout = ({ sections, backgroundImageSrc }) => {
       {sections.map((section, index) => (
         <div
           key={index}
-          className="lg:mb-12 text-white backdrop-filter lg:p-6 w-full"
+          className="lg:mb-12 text-white backdrop-filter lg:p-6 p-2 w-full"
         >
-          <div className="flex items-center justify-between">
+          <div className="flex items-center mb-28 lg:mb-0 justify-between">
             <h2 className="text-3xl font-bold lg:mb-4">{section.title}</h2>
             <p className="text-lg max-w-[40%] lg:max-w-[20%]">{section.description}</p>
           </div>
           <div className="w-full lg:mt-[10%] flex">
             <div className="flex overflow-x-auto lg:w-full justify-between">
               {section.details.map((detail, idx) => (
-                <div key={idx} className="mt-4 flex-1 min-w-[30%] border-l flex flex-col justify-between lg:max-w-[25%] p-3">
+                <div key={idx} className="mt-4 flex-1 min-w-[40%] border-l flex flex-col justify-between lg:max-w-[25%] p-3">
                   <h3 className="lg:text-xl font-semibold">{detail.subtitle}</h3>
                   <p className="lg:mt-[50%] mt-[10%] text-5xl font-extrabold">{detail.text}</p>
                 </div>
