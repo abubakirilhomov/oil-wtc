0595efba1fa1663ff7f9b4c2338792741ab84d80
bohodirovabdulloh <abdullohbohodirov967@gmail.com>
2025-01-28 18:03:16 +0500

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,28 @@ module.exports = {
         'afacad-flux': ['"Afacad Flux"', 'sans-serif'],
       },
       colors: {
-        background: "var(--background)",
         foreground: "var(--foreground)",
-        primaryBlue: "#022465"
+        primaryBlue: "#022465",
       },
-
     },
   },
-  plugins: [
-    require('daisyui'),
-  ]
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#022465",
+          "secondary": "#2563eb",
+          "accent": "#4f46e5",
+          "neutral": "#1f2937",
+          "base-100": "#ffffff", // Дефолтный белый фон
+          "info": "#38bdf8",
+          "success": "#22c55e",
+          "warning": "#facc15",
+          "error": "#ef4444",
+        },
+      },
+      "light", // Включает стандартную светлую тему
+    ],
+  },
 };
